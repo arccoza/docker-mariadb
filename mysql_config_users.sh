70cc21e9b89f04aad57a3e81aee288407ff284a8
adrien <adrien@arccoza.com>
2015-10-09 21:15:41 +0900

    Working Mariadb 10.0 docker image.

---
+++ mysql_config_users.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+mysqld --skip-grant-tables > /dev/null 2>&1 &
+
+RET=1
+while [[ RET -ne 0 ]]; do
+	echo "=> Waiting for confirmation of MariaDB service startup"
+	sleep 5
+	mysql -uroot -e "status" > /dev/null 2>&1
+	RET=$?
+done
+
+if [ ! -z "$MYSQL_ROOT_PASSWORD" ]; then
+	echo "=> Setting root password."
+	mysql -uroot -e "USE mysql; UPDATE user SET password=PASSWORD('$MYSQL_ROOT_PASSWORD') where User='root';"
+fi
+
+if [ ! -z "$MYSQL_DATABASE" ]; then
+	echo "=> Creating $MYSQL_DATABASE."
+	mysql -uroot -e "CREATE DATABASE IF NOT EXISTS \`$MYSQL_DATABASE\`;"
+fi
+
+if [ ! -z "$MYSQL_USER" -a ! -z "$MYSQL_PASSWORD" -a ! -z "$MYSQL_DATABASE" ]; then
+	echo "=> Creating $MYSQL_USER and granting privileges on $MYSQL_DATABASE."
+	mysql -uroot -e "CREATE USER '"$MYSQL_USER"'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';"
+	mysql -uroot -e "GRANT ALL PRIVILEGES ON '"$MYSQL_DATABASE"'.* TO '$MYSQL_USER'@'%' WITH GRANT OPTION;"
+fi
+
+echo "=> Done!"
+
+mysqladmin -uroot shutdown
---
+++ run.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Change bind address to 0.0.0.0
+sed -i -r 's/bind-address.*$/bind-address = 0.0.0.0/' /etc/mysql/my.cnf
+# Change the innodb_buffer_pool_size (default is 256M, INNODB_BUFFER_POOL_SIZE default is 128M).
+sed -i -e 's/^innodb_buffer_pool_size\s*=.*/innodb_buffer_pool_size = '$INNODB_BUFFER_POOL_SIZE'/' /etc/mysql/my.cnf
+
+VOLUME_HOME="/var/lib/mysql"
+
+if [ ! -d $VOLUME_HOME/mysql ]; then
+	mkdir -p "$VOLUME_HOME"
+	chown -R mysql:mysql "$VOLUME_HOME"
+	echo "=> An empty or uninitialized MariaDB volume is detected in $VOLUME_HOME"
+	echo "=> Installing MariaDB ..."
+	mysql_install_db > /dev/null 2>&1
+	echo "=> Done!"  
+	/mysql_config_users.sh
+else
+	echo "=> Using an existing volume of MariaDB"
+fi
+
+exec mysqld
